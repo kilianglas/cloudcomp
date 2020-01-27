0e068db07aa3b62c35909eb01f6029e42151b16b
Kilian Glas <glas_k@yahoo.de>
2020-01-27 23:21:20 +0100

    Bug fix.

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ def outliers_iqr(ys):
     ##############################################################################
     quartile_1, quartile_3 = np.percentile(ys, [25, 75]) 
     iqr = quartile_3 - quartile_1 
-    lower_bound = q1 - 1.5 * iqr 
-    upper_bound = q3 - 1.5 * iqr 
+    lower_bound = quartile_1 - 1.5 * iqr 
+    upper_bound = quartile_3 - 1.5 * iqr 
     return np.where((ys > upper_bound) | (ys < lower_bound))
 
 
